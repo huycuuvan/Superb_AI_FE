d0bf15782ef0622ff088a7c0524f2e075aec1799
Dinh Huy <ndhuy0904@gmail.com>
2025-05-24 16:09:00 +0700

    delete logo lovable

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,15 +8,13 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 3001,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
-      }
-    }
+      },
+    },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
