af32e09fae4e40d043c4dd4b0f003faa8d4b75de
Dinh Huy <ndhuy0904@gmail.com>
2025-07-25 10:06:50 +0700

    hotfix: build

--- src/components/TradingViewWidget.jsx
+++ src/components/TradingViewWidget.jsx
@@ -1,6 +1,6 @@
 // src/components/TradingViewWidget.jsx
 import React, { useEffect, useRef, memo } from 'react';
-import useIsMobile from '@/hooks/use-mobile';
+import {useIsMobile} from '@/hooks/use-mobile';
 
 function normalizeInterval(interval) {
   if (!interval) return 'D';
